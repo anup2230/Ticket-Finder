4b4fece64223aecdab3a4e0ddcaf3fd3f609e260
anup2230 <42153092+anup2230@users.noreply.github.com>
2023-05-04 03:22:25 -0500

    Update slack_sender.py

--- slack_sender.py
+++ slack_sender.py
@@ -1,21 +1,26 @@
 import os, requests, time
-from slack_sdk import WebClient
-from slack_sdk.errors import SlackApiError
+
 
 # Seatgeek API Credentials
 MYCLIENTID = "MjY4NzAwNDN8MTY4MDc2NDY4OS4zNDAyMTE"
 MYCLIENTSECRET = "08344d82fc87139119f1f64716e1186caa6fea46897e21ca43603a2c0d34b761"
 
-# Slack API Credentials
-SLACK_BOT_TOKEN = 'xoxb-5220496871392-5196826432690-fjtvtMid6NiDZbCRQZpdVhJ8'
-SLACK_CHANNEL_NAME = '#taylor-swift-ticket-bot'
+#Discord Data to Send
+payload = {
+    'content' : "hello"
+}
+#Discord Auth
+header = {
+    'authorization' : "Mzk4NjgzMDkxMjM0OTc5ODUy.G2cqyG.zefXL2zo4vGVYNh38IVFcdGKtO5GPy--DAdEiU"
+}
 
+#artist to search for
 artist = "taylor+swift"
 
+#API CALL URL
 URL = 'https://api.seatgeek.com/2/events?q={}&per_page=1000&client_id={}&client_secret={}'.format(artist, MYCLIENTID, MYCLIENTSECRET)
 
 page = requests.get(URL)
-client = WebClient(token=SLACK_BOT_TOKEN)
 result = ""
 
 while True: 
@@ -29,16 +34,8 @@ while True:
         venue = event['venue']['name']
         city = event['venue']['city']
 
-        if event_title.startswith("Taylor Swift with") and (lowest_price <= 1200):
-            result = event_title + ": Lowest Price = " + str(lowest_price) + venue + "|" + city + "\n" + event['url']
-            try:
-                response = client.chat_postMessage(
-                    channel=SLACK_CHANNEL_NAME,
-                    text=result
-                )
-                print("Message sent: ", response["ts"])  # ts is the timestamp of the message
-            except SlackApiError as e:
-                print("Error sending message: {}".format(e))
-            print(result)
-        time.sleep(1)    
-    time.sleep(120)
+        if event_title.startswith("Taylor Swift with") and (lowest_price <= 1150):
+            payload['content'] = event_title + ": Lowest Price = " + str(lowest_price) + " , " + venue + " | " + city + "\n" + event['url']
+            requests.post("https://discord.com/api/v9/channels/1103593266189975625/messages", data=payload, headers=header)
+            print(payload['content'])
+    time.sleep(60)
